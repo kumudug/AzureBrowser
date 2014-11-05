0935733b143b6c63b819df84df0dbe91e1abe785
kumudug <kumudug1@gmail.com>
2014-11-05 14:41:28 +1100

    Add azure settings

---
+++ AzureBrowserApp/WriteSampleTestData/AzureSettings.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteSampleTestData
+{
+    public class AzureSettings
+    {
+        private static AzureSettings _instance;
+        private CloudStorageAccount cloudStorageAccount;
+
+        public CloudStorageAccount TestStorageAccount
+        {
+            get
+            {
+                return cloudStorageAccount;
+            }
+        }
+        private AzureSettings()
+        {
+            cloudStorageAccount = new CloudStorageAccount(new StorageCredentials(Properties.Settings.Default.StorageAccountName, Properties.Settings.Default.PrimaryAccessKey), true);
+        }
+
+        public static AzureSettings Instance()
+        {
+            if (_instance == null)
+                _instance = new AzureSettings();
+            return _instance;
+        }
+    }
+}
--- AzureBrowserApp/WriteSampleTestData/Program.cs
+++ AzureBrowserApp/WriteSampleTestData/Program.cs
@@ -12,49 +12,12 @@ namespace WriteSampleTestData
     {
         static void Main(string[] args)
         {
-            var tblRows = Util.GetTableEntities(10);
+            var tblRows = Util.GetTableEntities(1000);
+            //AzureSettings.i
 
             var builder = new ContainerBuilder();
             builder.RegisterInstance(tblRows).As<List<SampleTableEntity>>();
-
-
-
-            // Open the configuration file and retrieve  
-            // the connectionStrings section.
-            Configuration config = ConfigurationManager.
-                OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
-
-            //ConnectionStringsSection section = config.GetSection("userSettings") as ConnectionStringsSection;
-
-            ConfigurationSection section = config.GetSection("userSettings/WriteSampleTestData.Properties.Settings");
-            //section.SectionInformation.UnprotectSection();
-            //section.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
-
-            section.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
-
-            // Save the encrypted section.
-            section.SectionInformation.ForceSave = true;
-
-            config.Save(ConfigurationSaveMode.Full);
-
-
-            //if (section.SectionInformation.IsProtected)
-            //{
-            //    // Remove encryption.
-            //    section.SectionInformation.UnprotectSection();
-            //}
-            //else
-            //{
-            //    // Encrypt the section.
-            //    section.SectionInformation.ProtectSection(
-            //        "DataProtectionConfigurationProvider");
-            //}
-            // Save the current configuration.
-            //config.Save();
-
-            Console.WriteLine("Protected={0}",
-                section.SectionInformation.IsProtected);
-
+            builder.RegisterInstance(AzureSettings.Instance()).As<AzureSettings>().SingleInstance();
 
             Console.ReadKey();
         }
