6f99074d88099f2f37b4f6784226f90e9cd4a49c
kumudug <kumudug1@gmail.com>
2014-11-05 19:11:53 +1100

    Api skeleton for Azure Tables

--- AzureBrowserApp/AzureBrowserApp/Controllers/AzureTableController.cs
+++ AzureBrowserApp/AzureBrowserApp/Controllers/AzureTableController.cs
@@ -13,17 +13,45 @@ namespace AzureBrowserApp.Controllers
         /// <summary>
         /// Gets all rows [maximum 500]
         /// </summary>
-        /// <returns></returns>
-        [Route("api/azuretable/getallrows")]
-        public IEnumerable<TableEntity> GetTableEntities()
+        /// <returns>IEnumerable<TableEntity> list of rows</returns>
+        [Route("api/azuretable/{tableName}/getallrows")]
+        public IEnumerable<TableEntity> GetTableEntities(string tableName)
         {
             return null;
         }
 
-        [Route("api/azuretable/{pageSize:int=20}/{pageNo:int=0}")]
-        public IEnumerable<TableEntity> GetTableEntities(int pageSize, int pageNo)
+        /// <summary>
+        /// Returns a subset of table rows
+        /// </summary>
+        /// <param name="tableName">table name</param>
+        /// <param name="pageSize">page size</param>
+        /// <param name="pageNo">page number</param>
+        /// <returns>IEnumerable<TableEntity> list of rows</returns>
+        [Route("api/azuretable/{tableName}/{pageSize:int=20}/{pageNo:int=0}")]
+        public IEnumerable<TableEntity> GetTableEntities(string tableName, int pageSize, int pageNo)
         {
             return null;
+        }
+
+        /// <summary>
+        /// Returns a list of all tables in the azure table storage
+        /// </summary>
+        /// <returns>IEnumerable<string> of table names</returns>
+        [Route("api/azuretable/getalltables")]
+        public IEnumerable<string> GetAllTables()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Returns the number of rows of the given table
+        /// </summary>
+        /// <param name="tableName">table name</param>
+        /// <returns>number or rows in table</returns>
+        [Route("api/azuretable/{tableName}")]
+        public int GetTableRowCount(string tableName)
+        {
+            return 0;
         }
     }
 }
